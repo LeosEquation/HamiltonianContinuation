3e2ac2df650b72a3da07105912dce8b7668f6ecb
Leonel Mayorga López <leoml141.lml@gmail.com>
2026-08-27 11:12:53 -0600

    Reorganizing

--- src/objects/objects.jl
+++ src/objects/objects.jl
@@ -1,9 +1,11 @@
 # HamiltonianContinuation/src/objects/objects.jl
 
-include("branchpoint_branch.jl")
-include("equilibrium_bifurcations.jl")
 include("equilibrium_branch.jl")
-include("flippoint_branch.jl")
+include("equilibrium_bifurcations.jl")
 include("limitpoint_branch.jl")
-include("orbit_bifurcations.jl")
+include("branchpoint_branch.jl")
+
 include("orbit_branch.jl")
+include("orbit_bifurcations.jl")
+include("flippoint_branch.jl")
+
